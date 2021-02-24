2b48383bb68e3d3f14512341f93f42313086176a
Mateusz Pawlowski <g00361162@gmit.ie>
2021-02-24 13:42:21 +0000

    Working with sizes of ints and chars

--- bitoperations.c
+++ bitoperations.c
@@ -2,9 +2,25 @@
 
 int main(int argc, char *argv[]){
     
-    // Hello World
-    printf("%s\n", "Hello World!");
+    int i = 0xF1; // 11110001
 
+    printf("Dec: %d\n", i);
+    printf("Hex: %X\n", i);
+
+    printf("Size of int: %d\n", sizeof(i));
+    printf("Size of int: %d\n", sizeof(int));
+    printf("Size of int: %d\n", sizeof(char));
+
+    char c = 41;
+    printf("C in char is: %c\n", c);
+    printf("C in int is: %d\n", c);
+
+    int j = 1000000000;
+    printf("J in char is: %d\n", j);
+    printf("J in int is: %c\n", (char) j);
+    printf("J int from char is: %d\n", (int) (char) j);
+
+    
     return 0;
 }
  
