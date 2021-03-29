130a81a56e43058543316e1f059f3d7cffe53cf0
Mateusz Pawlowski <g00361162@gmit.ie>
2021-03-29 21:06:14 +0100

    Ch function in C

---
+++ funcs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define WORD uint32_t
+#define PF PRIX32
+
+WORD Ch(WORD x, WORD y, WORD z){
+    return (x&y)^(~x&z);
+}
+
+int main(int argc, char *argv[]) {
+
+    uint32_t x = 0xf0f0f0f0;
+    uint32_t y = 0x0f0f0f0f;
+    uint32_t z = 0xf0f0f0f0;
+
+    WORD ans = Ch(x, y, z);
+
+    printf("Ch(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
+
+    return 0;
+}
