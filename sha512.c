ec7fc2a64cd1c34735fa4a19522938f573105225
Mateusz Pawlowski <g00361162@gmit.ie>
2021-04-28 14:25:01 +0100

    Added if to make sure file was put in

--- sha512.c
+++ sha512.c
@@ -190,22 +190,29 @@ int main(int argc, char *argv[]) {
         0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
         0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
     };
+    
+    
 
-    // File pointer for reading
-    FILE *f;
-    // Open file from command line for reading
-    f = fopen(argv[1], "r");
+    if(argc < 2){
+        printf("No files was inputed, please enter a file");
+    } else{   
+        
+        // File pointer for reading
+        FILE *f;
+        // Open file from command line for reading
+        f = fopen(argv[1], "r");
 
-    // Calculate the SHA512 of f
-    sha512(f, H);
+        // Calculate the SHA512 of f
+        sha512(f, H);
 
-    // Print the final SHA512 hash
-    for (int i = 0; i < 8; i++)
-        printf("%016" PF, H[i]);
-    printf("\n");
+        // Print the final SHA512 hash
+        for (int i = 0; i < 8; i++)
+            printf("%016" PF, H[i]);
+        printf("\n");
 
-    // Close the file
-    fclose(f);
+        // Close the file
+        fclose(f);
 
+    }
     return 0;
 }
--- tests.sh
+++ tests.sh
@@ -4,8 +4,8 @@ echo ""
 echo "Checking file input.txt"
 expected="$(sha512sum input.txt)"
 actual="$(./sha512 input.txt)  input.txt"
-echo "Expected: " $expected
-echo "Actual:   " $actual
+echo "Expected:" $expected
+echo "Actual:  " $actual
 if [[ $actual == $expected ]]; then 
     echo "Pass"
 else
@@ -16,8 +16,8 @@ echo ""
 echo "Checking file empty.txt"
 expected="$(sha512sum empty.txt)"
 actual="$(./sha512 empty.txt)  empty.txt"
-echo "Expected: " $expected
-echo "Actual:   " $actual
+echo "Expected:" $expected
+echo "Actual:  " $actual
 if [[ $actual == $expected ]]; then 
     echo "Pass"
 else
